59312d866470af576af0e58dd58e74581869aeec
Donovan Buck <donovan@donovan.bz>
2013-03-11 10:57:58 -0500

    Updates

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
-console.log('This would be the main JS file.');
+jQuery((function($) {
+	$('.screenshots li').each( function( idx, item ) {
+		var $item = $(item),
+			name = $item.attr('rel'),
+			link = $item.find('a').attr('href'),
+			title = $item.find('a').html();
+			
+		$item.html('<img src="images/screenshots/thumb/' + name + '.png" width="215" /><br />' + title + '<div class="title"></div>');
+	});
+})(jQuery));
